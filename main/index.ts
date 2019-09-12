52b31d8188f258b3b6818d659497396469ee97c3
Charlie Saunders <charlieasaunders@gmail.com>
2019-09-12 09:56:30 -0400

    Shorter intervals.

--- main/index.ts
+++ main/index.ts
@@ -4,4 +4,4 @@ function testSubtract(a: number, b: number): number {
   return subtract(a, b)
 }
 
-setInterval(() => console.log(`testSubtract(19, 3) = ${testSubtract(19, 3)}`), 10000)
+setInterval(() => console.log(`testSubtract(19, 3) = ${testSubtract(19, 3)}`), 1000)
--- main2/index.ts
+++ main2/index.ts
@@ -4,4 +4,4 @@ function testAdd(a: number, b: number): number {
   return add(a, b)
 }
 
-setInterval(() => console.log(`testSubtract(19, 3) = ${testAdd(19, 3)}`), 10000)
+setInterval(() => console.log(`testAdd(19, 3) = ${testAdd(19, 3)}`), 1000)
